c83ca94d42dc6c68ac1161d33b82bc2f427e6611
root <juandiemore@gmail.com>
2019-06-12 22:06:59 -0500

    display UID en menu

--- ionic/src/app/services/auth.service.ts
+++ ionic/src/app/services/auth.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth  } from "@angular/fire/auth";
 import { Router } from "@angular/router";
-
+import {of as observableOf } from 'rxjs';
+import { map,filter } from 'rxjs/operators';
+import {auth} from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  // user = observableOf('123');
+  user = this.AFauth.authState.pipe(
+    map(authState => {
+      if(!authState){
+        return null;
+      }else
+      return  authState.uid;
+    }),
+  )
   constructor(private AFauth: AngularFireAuth, private router:Router) { }
 
   Login(email:string, password:string){
@@ -16,9 +26,8 @@ export class AuthService {
         resolve(user);
       }).catch(err => rejected(err));
     });
-
-
   }
+
   logout(){
     this.AFauth.auth.signOut().then(auth =>{
       this.router.navigate(["/login"]);
