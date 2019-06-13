27915d124aa77a75a7d7a57774ce5a472ee45180
root <juandiemore@gmail.com>
2019-06-13 12:08:48 -0500

    organizado

---
+++ firebase/db_functions.js
@@ -0,0 +1,28 @@
+evento = {
+  Nombre : '',
+  Lugar : '',
+  ID_usuario:'',
+  lat:0,
+  lon:0
+};
+
+
+avatar = {
+  ID_usuario:'',
+  Cabello:0,
+  Cara :0
+}
+
+usuario={
+  email:'',
+  password:'',
+  Username:'',
+  foto:'',
+  celular:''
+}
+
+grupo = {
+  ID_lider:'',
+  ID_usuario:'',
+  ID_evento:''
+}
